95bb0e2f00127ccf671c6865e71dc0c68c783d4b
Vadim Khasanshin <vadim.kh7@gmail.com>
2022-07-25 17:15:44 +0400

    Revert "log new users"
    This reverts commit 17008603dd1fde54a5f96d6fe5d7a6cbf99c304e.

---
+++ bot/config.py
@@ -0,0 +1,13 @@
+import logging
+import os
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+TOKEN = os.environ.get('VPN_BOT_TOKEN', '')
+
+QIWI_P2P = os.environ.get('QIWI_P2P', '')
+
